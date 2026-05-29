cda8b19d58009bc46b87ab72af669026cef20236
Alexandre Bendlin <133797103+alexbendlin@users.noreply.github.com>
2026-05-29 14:19:16 +0000

    feat: configuração do nodemon para live reload e automação com o script dev

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,24 @@ app.get('/', function(requisicao, resposta) {
     resposta.send("🚀 API do Gerenciador de Tarefas rodando com Express com sucesso!");
 });
 
+// ==========================================================================
+// ROTA ADICIONAL (RESOLUÇÃO DO DESAFIO)
+// ==========================================================================
+
+/* Criando o endpoint '/status' que serve para monitorar a integridade 
+   e a saúde do nosso ecossistema de backend */
+app.get('/status', function(requisicao, resposta) {
+    /* O método .json() é uma evolução do .send(). Ele avisa automaticamente 
+       ao navegador do cliente (através do cabeçalho Content-Type: application/json)
+       que o dado enviado é um objeto estruturado e não apenas texto puro. */
+    resposta.json({
+        status: "Operacional",
+        versao: "1.0.0",
+        ambiente: "GitHub Codespaces",
+        dataVerificacao: new Date().toLocaleDateString("pt-BR")
+    });
+});
+
 // 5. Ligando o servidor para escutar as requisições na porta definida
 app.listen(PORTA, function() {
     console.log(`==================================================`);
